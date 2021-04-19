56abada9445f87050c9aa21aded1231c3e8cd6d9
nickmckay <nicholas.mckay@nau.edu>
2021-04-19 14:03:39 +1200

    excursion added, CI,

--- R/excursion.R
+++ R/excursion.R
@@ -1,38 +1,53 @@
-#' maximum number of consecutive TRUE values in a vector
+#' Maximum number of consecutive values in a vector
 #'
-#' @param bool
+#' @param vec a vector
+#' @param val the value to search for consecutive values
+#'
 #' @concept https://stackoverflow.com/questions/37345853/find-the-maximum-and-mean-length-of-the-consecutive-true-arguments
-#' @return
+#' @return the number of consecutive values. 0 means that value is not present in the vector
 #' @export
-maxConsecutiveTRUE <- function(bool){
+#' @examples
+#' maxConsecutive(c(1,2,7,5,7,7,7,3,4),val = 7)
+maxConsecutive <- function(vec,val = TRUE){
 
-  consec <- rle(bool)
+  consec <- rle(vec)
 
   # max consecutive values
-  mv <- max(consec$lengths[consec$values==TRUE])
+  mv <- max(consec$lengths[consec$values==val])
   if(!is.finite(mv)){
     mv <- 0
   }
 
-  return(mv)
+  if(mv > 0){
+    #get first indices for max
+    fi <- min(which(consec$lengths == mv & consec$values == val))
+    io <- seq(cumsum(consec$lengths)[fi]-(mv-1),cumsum(consec$lengths)[fi])
+  }else{
+    io <- NA
+  }
+
+  return(list(max = mv,index = io))
 }
 
 
-#' Title
+#' Detect an excursion in a timeseries
 #'
-#' @param age
-#' @param vals
-#' @param event.yr
-#' @param event.window
-#' @param ref.window
-#' @param n.consecutive
-#' @param output.figure.path
-#' @param sigNum
+#' @author Hannah Kolus
+#' @author Nick McKay
+#' @references Morrill
+#' @param age a year or age vector
+#' @param vals a vector of values the same length as age
+#' @param event.yr the center of the proposed excursion window
+#' @param event.window the width of the proposed excursion window
+#' @param ref.window how many years to use as a reference before and after the event window
+#' @param n.consecutive how many consecutive points are required for this to be considered an excursion? (default = 2)
+#' @param output.figure.path path pointing to where should the output figure be saved? An NA will not produce a figure (default = NA)
+#' @param sig.num how many standard deviations required outside the reference windows must be exceeded for this to be considered an excursion? (default = 2)
 #'
-#' @return
+#' @importFrom stats lm predict sd
+#'
+#' @return a tibble that describes the positive and negative excursion results
 #' @export
-#'
-#' @examples
 detectExcursion = function(age,
                            vals,
                            event.yr,
@@ -80,7 +95,7 @@ detectExcursion = function(age,
   postSD = sd(values[post.i[-extremeInd[1]]])
 
   # Identify mean and sd used to test the high/positive anomaly threshold
-  if (preAVG + sigNum * preSD > postAVG + sigNum * postSD) {
+  if (preAVG + sig.num * preSD > postAVG + sig.num * postSD) {
     sd.hi = preSD
     avg.hi = preAVG
   } else {
@@ -89,7 +104,7 @@ detectExcursion = function(age,
   }
 
   # Identify mean and sd used to test the low/negative anomaly threshold
-  if (preAVG - sigNum * preSD < postAVG - sigNum * postSD) {
+  if (preAVG - sig.num * preSD < postAVG - sig.num * postSD) {
     sd.lo = preSD
     avg.lo = preAVG
   } else {
@@ -98,39 +113,51 @@ detectExcursion = function(age,
   }
 
   # Identify points in event window that exceed the thresholds defined above
+  # first look for positive excursions
 
-  aboveBool <- values[event.i] > avg.hi + sigNum * sd.hi
-  belowBool <- values[event.i] < avg.lo - sigNum * sd.lo
-
+  aboveBool <- values[event.i] > avg.hi + sig.num * sd.hi
+  mctAbove <- maxConsecutive(aboveBool)
   abovePts = which(aboveBool)
-  belowPts = which(belowBool)
 
   # Determine whether there are any consecutive extreme points - this qualifies an event
-   if (maxConsecutiveTRUE(aboveBool) >= n.consecutive | maxConsecutiveTRUE(belowBool) >= n.consecutive) {
-    eventEX = TRUE
+  if (mctAbove$max >= n.consecutive) {
+    valuesEX = age[mctAbove$index]
 
-    # Check direction
-    indsA = which(diff(abovePts) == 1)
-    indsB = which(diff(belowPts) == 1)
-
-    if (length(indsA) > 0 && length(indsB) > 0) {
-      diffA = max(values[event.i[abovePts[c(indsA, indsA+1)]]] - (avg.hi + sigNum * sd.hi))
-      diffB = max((avg.lo - sigNum * sd.lo) - values[event.i[belowPts[c(indsB, indsB+1)]]])
-      dirEX = ifelse(diffA > diffB, 1, -1)
-    } else {
-      dirEX = ifelse(length(indsA) > 0, 1, -1)
-    }
-
-    # find event onset
-    onsetEX = ifelse(dirEX == 1, age[event.i[abovePts[indsA[length(indsA)]+1]]], age[event.i[belowPts[indsB[length(indsB)]+1]]])
-
+    outRowAbove <- tibble::tibble(excursionDetected = TRUE,
+                             excursionDirection = 1,
+                             excursionValues = list(valuesEX))
+  }else{
+    outRowAbove <- tibble::tibble(excursionDetected = FALSE,
+                                  excursionDirection = 1,
+                                  excursionValues = NA)
   }
+
+  belowBool <- values[event.i] < avg.lo - sig.num * sd.lo
+  mctBelow <- maxConsecutive(belowBool)
+
+  # Determine whether there are any consecutive extreme points - this qualifies an event
+  if (mctBelow$max >= n.consecutive) {
+    # find event onset and termination
+    valuesEX = age[mctAbove$index]
+
+    outRowBelow <- tibble::tibble(excursionDetected = TRUE,
+                                  excursionDirection = -1,
+                                  excursionValues = list(valuesEX))
+  }else{
+    outRowBelow <- tibble::tibble(excursionDetected = FALSE,
+                                  excursionDirection = -1,
+                                  excursionValues = NA)
+  }
+
 
   if (!is.na(output.figure.path)) {
 
-  #plotExcursion() TBD
+    #plotExcursion() TBD
   }
 
-  return(list(statusEX, eventEX, dirEX, onsetEX))
+  #prepare output tibblee
+  out <- dplyr::bind_rows(outRowAbove,outRowBelow)
+
+  return(out)
 
 }
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # actR
 Development repository for the abrupt change toolkit in R. 
+
+<!-- badges: start -->
+  [![R build status](https://github.com/LinkedEarth/actR/workflows/R-CMD-check/badge.svg)](https://github.com/LinkedEarth/actR/actions)
+  <!-- badges: end -->
