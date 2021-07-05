7a4dae4748b66fefe6f2c78f0e570fc8f780fa4a
CommonClimate <julieneg@usc.edu>
2021-07-05 11:07:37 -0700

    Update synth.R

--- R/synth.R
+++ R/synth.R
@@ -9,10 +9,11 @@
 #' @export
 
 linearRamp <-function(lngth,width){
-  if (width>lngth/2) {stop("Ramp width cannot exceed half of the length")}
-  xs = seq(lngth)/lngth-1/2
-  ramp = 4*fBasics::Ramp(xs,a = -width/lngth)
-  ramp[xs>width/lngth] = 1.0
+  if (width>lngth) {stop("Ramp width cannot exceed the length")}
+  ramp = numeric(length=lngth)  # sets zeros everywhere
+  x = seq(lngth)
+  ramp[x>lngth/2+width/2] = 1.0
+  ramp[x>lngth/2-width/2 & x <= lngth/2+width/2] = seq(width)/width
   return(ramp)
 }
 
